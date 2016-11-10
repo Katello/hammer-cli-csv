cea2c1b929f17c4e33a9d28cf758dd71fc932c29
Andrew Kofink <ajkofink@gmail.com>
2016-11-10 08:01:10 -0500

    Fixes #17307 - move help module out of CsvCommand (#137)

--- lib/hammer_cli_csv/csv.rb
+++ lib/hammer_cli_csv/csv.rb
@@ -2,24 +2,24 @@ require 'hammer_cli'
 require 'hammer_cli/exit_codes'
 
 module HammerCLICsv
+  module Help
+    class Builder < HammerCLI::Help::Builder
+      def add_list(heading, items)
+        items.delete_if do |item|
+          if item.class == Clamp::Subcommand::Definition
+            !item.subcommand_class.supported?
+          else
+            false
+          end
+        end
+        super(heading, items)
+      end
+    end
+  end
+
   class CsvCommand < HammerCLI::AbstractCommand
     def help
       self.class.help(invocation_path, HammerCLICsv::Help::Builder.new)
-    end
-
-    module Help
-      class Builder < HammerCLI::Help::Builder
-        def add_list(heading, items)
-          items.delete_if do |item|
-            if item.class == Clamp::Subcommand::Definition
-              !item.subcommand_class.supported?
-            else
-              false
-            end
-          end
-          super(heading, items)
-        end
-      end
     end
   end
 
